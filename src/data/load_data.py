7a258840c2a7d5096685114c187f2d3efa1661e7
S-Pz <savio_fr@outlook.com>
2026-03-26 22:35:02 -0300

    Changing dataset load

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,7 +2,7 @@ import chardet
 import pandas as pd
 
 def loadDataEnem(filePath):
-    row_number = 300000
+    row_number = 100000
     with open(filePath,'rb') as f:
         result = chardet.detect(f.read(row_number))
     
